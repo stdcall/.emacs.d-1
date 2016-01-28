61493039e3279157273f68cc1ce7831d876e17c0
Jonathan <jagrg@users.noreply.github.com>
2016-01-28 17:25:56 +0000

    Change file name

--- lisp/transcribe.el
+++ lisp/transcribe.el
@@ -1,8 +1,8 @@
-;;; transcribe-mode.el --- a tool for transcribing audio with expressscribe in emacs -*- lexical-binding: t; -*-
+;;; transcribe.el --- a tool for transcribing audio with ExpressScribe in Emacs -*- lexical-binding: t; -*-
 
 ;;; Comentary:
 
-;; I set ExpressScribe to use the following key bindings:
+;; ExpressScribe uses the following key bindings:
 
 ;; | C-d   | rewind                       |
 ;; | C-j   | play                         |
@@ -16,33 +16,32 @@
 ;;; Code:
 
 (require 'key-chord)
-(key-chord-mode 1)
 (require 'auto-capitalize)
-(add-hook 'transcribe-mode-hook #'auto-capitalize-mode)
+(key-chord-mode 1)
+(add-hook 'transcribe-hook #'auto-capitalize-mode)
 
-(defgroup transcribe-mode nil
+(defgroup transcribe nil
   "Customization group for transcribe minor mode.")
 
-(define-minor-mode transcribe-mode
-  "A minor mode for editing transcriptions using emms."
+(define-minor-mode transcribe
+  "A minor mode for making transcriptions."
   :lighter " Transcribe"
   :keymap (let ((map (make-sparse-keymap)))
-	    ;; (define-key map (kbd "M-t") 'transcribe-insert-ellipsis)
             map))
 
-(key-chord-define transcribe-mode-map "jk" 'transcribe-insert-interviewer)
-(key-chord-define transcribe-mode-map "jl" 'transcribe-insert-interviewee)
-(key-chord-define transcribe-mode-map ".," 'transcribe-insert-ellipsis)
+(key-chord-define transcribe-map "jk" 'transcribe-insert-interviewer)
+(key-chord-define transcribe-map "jl" 'transcribe-insert-interviewee)
+(key-chord-define transcribe-map ".," 'transcribe-insert-ellipsis)
 
 (defcustom transcribe-interviewer nil
-  "Define the name of the interviewer."
+  "The name of the interviewer."
   :type 'string
-  :group 'transcribe-mode)
+  :group 'transcribe)
 
 (defcustom transcribe-interviewee nil
-  "Define the name of the interviewee."
+  "The name of the interviewee."
   :type 'string
-  :group 'transcribe-mode)
+  :group 'transcribe)
 
 (defun transcribe-insert-interviewer ()
   "Insert the interviewer's name in a new paragraph."
@@ -57,27 +56,36 @@
   (insert (concat "\n\n" transcribe-interviewee ": ")))
 
 (defun transcribe-insert-ellipsis ()
-  "Insert ellipsis at point."
+  "Insert ellipsis."
   (interactive)
   (insert "(...) "))
 
-(defun transcribe-open-expressscribe ()
-  "Open ExpressScribe GUI."
+(defun transcribe-open-gui ()
+  "Open ExpressScribe."
   (interactive)
   (shell-command-to-string "open -a ExpressScribe"))
 
 (defun transcribe-initialize ()
-  "Enable `transcribe-mode' settings and open ExpressScribe."
+  "Enable `transcribe' settings and open GUI."
   (interactive)
-  (transcribe-mode)
-  (transcribe-open-expressscribe))
+  (transcribe)
+  (transcribe-open-gui))
 
 (defun transcribe-insert-link ()
-  "Insert link that opens ExpressScribe."
+  "Insert link that opens the GUI."
   (interactive)
   (insert "elisp:transcribe-initialize"))
 
-;; highlight
+(defun transcribe-new-name ()
+  "Prompt for a new name, replacing all occurances of
+\"Interviewee\"."
+  (interactive)
+  (let ((new-name (read-from-minibuffer "Enter new name: ")))
+    (insert (format "%s" new-name
+		    (let ((case-fold-search t))
+		      (goto-char (point-min))
+		      (while (search-forward "Interviewee" nil t)
+			(replace-match new-name t t)))))))
 
 (font-lock-add-keywords 'org-mode
                         '(("\\[[0-9]:[0-9][0-9]:[0-9][0-9]]" . font-lock-constant-face)))
@@ -88,21 +96,9 @@
 (font-lock-add-keywords 'org-mode
                         '(("Interviewee:" . font-lock-variable-name-face)))
 
-(defun transcribe-new-name ()
-  "Prompt user for a new name, replacing all occurances of
-`Interviewee'."
-  (interactive)
-  (let ((new-name (read-from-minibuffer "Enter new name: ")))
-    (insert (format "%s" new-name
-		    (let ((case-fold-search t))
-		      (goto-char (point-min))
-		      (while (search-forward "Interviewee" nil t)
-			(replace-match new-name t t)))
-	    ))))
-
 (setq auto-capitalize-predicate
       (lambda () (not (looking-back
 		       "\\([Ee]\\.g\\|[Ii]\\.e\\|\\.\\.\\)\\.[^.]*" (- (point) 20)))))
 
-(provide 'transcribe-mode)
-;;; transcribe-mode end here
+(provide 'transcribe)
+;;; transcribe ends here
