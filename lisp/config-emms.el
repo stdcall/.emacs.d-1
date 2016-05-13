bb6c6939032be5af41d0e9eafdaa22766a05a4a4
Jonathan <jagrg@users.noreply.github.com>
2016-05-13 12:30:21 +0100

    Add delimiters and cleanup

--- lisp/config-emms.el
+++ lisp/config-emms.el
@@ -1,22 +1,18 @@
-;;; config-emms.el --- emacs multi-media system configuration file
-
-;;; Installation:
-
-;; Add this file to your load-path and require it. You will also need
-;; mplayer and taglib installed on your system. Consider adding
-;; af=scaletempo to your .mplayer config file located in your home
-;; directory. This prevents the pitch from changing when the playback
-;; speed is altered.
+;;; config-emms.el --- Emacs Multi-Media System configuration file
 
 ;;; Commentary:
 
+;; This configuration uses mplayer and taglib. Note that adding
+;; af=scaletempo to the .mplayer config file prevents the pitch from
+;; changing when the playback speed is altered.
+;;
 ;; There are a few useful commands in the playlist buffer: C
 ;; emms-playlist-clear; E emms-tag-editor-edit; S t sort by title; S a
 ;; sort by artist; S N sort by name.
 ;;
-;; To delete a file from disk you will have to (1) S-m to enable
-;; emms-mark-mode; (2) d to open the file in a dired buffer; (3) d to
-;; delete the file; and (4) x to execute.
+;; To delete a file from disk: (1) S-m to enable emms-mark-mode; (2) d
+;; to open the file in a dired buffer; (3) d to delete the file; and
+;; (4) x to execute.
 
 ;;; Code:
 
@@ -34,25 +30,15 @@
 (require 'emms-mark)
 (require 'helm-emms)
 
+
+;; ==================================================================
+;; ˚˚ default settings
+;; ==================================================================
+
 (emms-standard)
 (emms-default-players)
 (emms-mode-line 1)
 (emms-playing-time 1)
-
-(defgroup config-emms nil
-  "Customization group for `config-emms'.")
-
-(defcustom emms-seconds-to-rewind -5
-  "Number of seconds to rewind. The default value is -5."
-  :type 'number
-  :group 'config-emms)
-
-(defcustom emms-seconds-to-fast-forward +5
-  "Number of seconds to fast forward. The default value is +5."
-  :type 'number
-  :group 'config-emms)
-
-(defvar emms-player-mplayer-speed-increment 0.1)
 
 (setq emms-playing-time-display-format "%s")
 (setq emms-tag-editor-rename-format "%t")
@@ -69,6 +55,18 @@
 (setq emms-info-functions '(emms-info-libtag))
 (setq emms-info-libtag-program-name "/usr/bin/emms-print-metadata")
 
+;; ==================================================================
+;; ˚˚ speed and seeking
+;; ==================================================================
+
+(defcustom emms-seconds-to-rewind -5
+  "Number of seconds to rewind. The default value is -5.")
+
+(defcustom emms-seconds-to-fast-forward +5
+  "Number of seconds to fast forward. The default value is +5.")
+
+(defvar emms-player-mplayer-speed-increment 0.1)
+
 (defun emms-rewind ()
   "Rewind audio incrementally in seconds.
 The value is specified in the variable `emms-seconds-to-rewind' and
@@ -83,10 +81,6 @@ a positive number."
   (interactive)
   (emms-seek emms-seconds-to-fast-forward))
 
-(defun emms-open-music-directory ()
-  (interactive)
-  (dired emms-source-file-default-directory))
-
 (defun emms-player-mplayer-speed-up ()
   "Depends on mplayer's -slave mode."
   (interactive)
@@ -100,28 +94,11 @@ a positive number."
   (process-send-string emms-player-simple-process-name
 		       (format "speed_incr %f\n" (- 0 emms-player-mplayer-speed-increment))))
 
-(defun emms-default-playlist ()
-  "Add music library to the playlist and go to it."
-  (interactive)
-  (emms-add-directory-tree emms-source-file-default-directory)
-  (emms))
+;; ==================================================================
+;; ˚˚ track and modeline display
+;; ==================================================================
 
-;; add tracks from dired
-
-(defun dired-add-to-emms-playlist nil
-  "Add directory tree to the current EMMS playlist."
-  (interactive)
-  (let ((file (expand-file-name (dired-get-filename))))
-    (if (and file (file-directory-p file))
-        (emms-add-directory-tree file)
-      (emms-add-file file))))
-
-(add-hook 'dired-mode-hook 'my-dired-mode-hook)
-
-(defun my-dired-mode-hook()
-  (define-key dired-mode-map (kbd "e") 'dired-add-to-emms-playlist))
-
-;; emms track display
+;; track display
 
 (defun jag/emms-track-description (track)
   (let* ((empty "...")
@@ -144,7 +121,7 @@ a positive number."
 
 (setq emms-track-description-function 'jag/emms-track-description)
 
-;; emms modeline display
+;; modeline display
 ;; http://metasyntax.net/guides/emacs.html
 
 (defvar emms-max-title-chars 36
@@ -172,17 +149,10 @@ modeline.")
 		    (format emms-mode-line-format (concat "Radio: " name)))
                 (format emms-mode-line-format "Unknown")))))))
 
-;; emms volume control
-;; see http://anthony.lecigne.net/blog/2014/08/16/control-volume-mplayer-emms/
+;; ==================================================================
+;; ˚˚ tag editor
+;; ==================================================================
 
-(defun jag/emms-volume-change-function (amount)
-  (interactive "p")
-  (process-send-string
-   emms-player-simple-process-name (format "volume %d\n" amount)))
-
-(setq emms-volume-change-function 'jag/emms-volume-change-function)
-
-;; emms tag editor
 ;; http://www.emacswiki.org/emacs/emms-extension.el
 
 (defun emms-tag-editor-next-same-field (&optional reverse)
@@ -214,7 +184,49 @@ buffer."
   (windmove-up)
   (delete-other-windows))
 
-;; key bindings
+;; ==================================================================
+;; ˚˚ misc utils
+;; ==================================================================
+
+(defun emms-open-music-directory ()
+  "Open music directory in a dired buffer."
+  (interactive)
+  (dired emms-source-file-default-directory))
+
+(defun emms-default-playlist ()
+  "Add music library to the playlist and go to it."
+  (interactive)
+  (emms-add-directory-tree emms-source-file-default-directory)
+  (emms))
+
+;; add tracks from dired
+
+(defun dired-add-to-emms-playlist nil
+  "Add directory tree to the current EMMS playlist."
+  (interactive)
+  (let ((file (expand-file-name (dired-get-filename))))
+    (if (and file (file-directory-p file))
+        (emms-add-directory-tree file)
+      (emms-add-file file))))
+
+(add-hook 'dired-mode-hook 'my-dired-mode-hook)
+
+(defun my-dired-mode-hook ()
+  (define-key dired-mode-map (kbd "e") 'dired-add-to-emms-playlist))
+
+;; volume control
+;; see http://anthony.lecigne.net/blog/2014/08/16/control-volume-mplayer-emms/
+
+(defun jag/emms-volume-change-function (amount)
+  (interactive "p")
+  (process-send-string
+   emms-player-simple-process-name (format "volume %d\n" amount)))
+
+(setq emms-volume-change-function 'jag/emms-volume-change-function)
+
+;; ==================================================================
+;; ˚˚ key bindings
+;; ==================================================================
 
 (global-set-key (kbd "M-p h") 'helm-emms)
 (global-set-key (kbd "M-p e") 'emms)
