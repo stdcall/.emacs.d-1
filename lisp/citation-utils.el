1660f33184144e77d1d59703fa77456f1b78c50a
Jonathan <jagrg@users.noreply.github.com>
2016-05-16 23:26:18 +0100

    Add keymap and citation formatter

--- lisp/citation-utils.el
+++ lisp/citation-utils.el
@@ -9,21 +9,63 @@
 (require 'org-ref)
 (require 'helm-bibtex)
 
-(defcustom power-ref-completion-actions
-  '(("Open PDF" . helm-bibtex-open-pdf)
-    ("Insert citation" . helm-bibtex-insert-citation) ; C-r (C-u C-r to insert prenotes)
-    ("Edit notes" . bibtex-completion-edit-notes)
-    ("Add keywords" . org-ref-helm-tag-entries)
-    ("Insert notes template\n" . power-ref-insert-notes-template)
-    ("Show entry" . bibtex-completion-show-entry)
-    ("Open URL or DOI" . helm-bibtex-open-url-or-doi)
-    ("Insert reference" . helm-bibtex-insert-reference)
-    ("Copy key" . power-ref-copy-key)
-    ("Attach PDF to email" . helm-bibtex-add-PDF-attachment))
+(defcustom power-ref-actions
+  '(("Open PDF              C-M-p"   . helm-bibtex-open-pdf)
+    ("Insert citation       C-M-c"   . helm-bibtex-insert-citation)
+    ("Edit notes            C-M-n"   . bibtex-completion-edit-notes)
+    ("Add keywords          C-M-k"   . power-ref-tag-entries)
+    ("Insert notes template C-M-i\n" . power-ref-insert-notes-template)
+    ("Show entry"                    . bibtex-completion-show-entry)
+    ("Open URL or DOI"               . helm-bibtex-open-url-or-doi)
+    ("Insert reference"              . helm-bibtex-insert-reference)
+    ("Copy key"                      . power-ref-copy-key)
+    ("Attach PDF to email"           . helm-bibtex-add-PDF-attachment))
   "Cons cells of string and function to set the actions of `helm-bibtex' to.
 The car of cons cell is the string describing the function. The cdr of
 the the cons cell is the function to use."
   :type '(alist :key-type string :value-type function))
+
+(defcustom power-ref-number-of-optional-arguments 2
+  "The number of optional arguments (aka pre and postnotes)."
+  :type 'integer)
+
+(defconst power-ref-preferred-number-of-optional-arguments
+  power-ref-number-of-optional-arguments)
+
+(defun power-ref-citation-format (keys)
+  "Formatter for `org-ref' citation commands.
+Prompt for the command and additional arguments if the commands can
+take any. To use this formatter, add it to
+`bibtex-completion-format-citation-functions'."
+  (let* ((initial (when bibtex-completion-cite-default-as-initial-input bibtex-completion-cite-default-command))
+         (default (unless bibtex-completion-cite-default-as-initial-input bibtex-completion-cite-default-command))
+         (default-info (if default (format " (default \"%s\")" default) ""))
+	 ;; (cite-command (helm-comp-read
+         ;;                (format "Cite command%s: " default-info)
+         ;;                bibtex-completion-cite-commands :must-match nil :initial-input initial)))
+	 (cite-command (completing-read (format "Cite command%s: " default-info)
+	 				bibtex-completion-cite-commands nil nil initial
+	 				'bibtex-completion-cite-command-history default nil)))
+    (if (member cite-command '("nocite" "supercite"))  ; These don't want arguments.
+        (format "%s:%s" cite-command (s-join "," keys))
+
+      (if (= power-ref-number-of-optional-arguments 0)
+          (format "%s:%s" cite-command (s-join "," keys))
+
+        (if (= power-ref-number-of-optional-arguments 1)
+            (let ((pos (if (= power-ref-number-of-optional-arguments 1)
+                           (read-from-minibuffer "Postnote[1]: ") "")))
+              (if (and (= power-ref-number-of-optional-arguments 1) (string= "" pos))
+                  (format "%s:%s" cite-command (s-join "," keys))
+                (format "[[%s:%s][%s]]" cite-command (s-join "," keys) pos)))
+
+          (let ((pre (if (= power-ref-number-of-optional-arguments 2)
+                         (read-from-minibuffer "Prenote[1]: ") ""))
+                (pos (if (= power-ref-number-of-optional-arguments 2)
+                         (read-from-minibuffer "Postnote[2]: ") "")))
+            (if (and (= power-ref-number-of-optional-arguments 2) (string= "" pre) (string= "" pos))
+                (format "%s:%s" cite-command (s-join "," keys))
+              (format "[[%s:%s][%s::%s]]" cite-command (s-join "," keys) pre pos))))))))
 
 ;; small alignment changes
 
@@ -89,6 +131,7 @@ keywords field."
 
 ;;;###autoload
 (defun power-ref-bad-citations ()
+  "Show bad citations, ref links and labels."
   (interactive)
   (let ((cb (current-buffer))
         (bad-citations (org-ref-bad-cite-candidates))
@@ -308,6 +351,40 @@ at the end of you file.
                                   (funcall x)))))
 	  :buffer "*power ref utils*")))
 
+;; keymap
+
+(defvar power-ref-map
+  (let ((map (make-sparse-keymap)))
+    (set-keymap-parent map helm-map)
+    ;; mnemonics
+    (define-key map (kbd "C-M-p") 'power-ref-open-pdf)
+    (define-key map (kbd "C-M-c") 'power-ref-insert-citation)
+    (define-key map (kbd "C-M-n") 'power-ref-edit-notes)
+    (define-key map (kbd "C-M-k") 'power-ref-tag-entries)
+    map))
+
+(defun power-ref-open-pdf ()
+  (interactive)
+  (helm-exit-and-execute-action 'helm-bibtex-open-pdf))
+
+(defun power-ref-insert-citation (arg)
+  "Insert citation at point.
+With a prefix ARG, prompt for pre and postnotes. See
+`power-ref-number-of-optional-arguments' for more information."
+  (interactive "P")
+  (if arg
+      (setq power-ref-number-of-optional-arguments 2)
+    (setq power-ref-number-of-optional-arguments
+	  power-ref-preferred-number-of-optional-arguments))
+  (helm-exit-and-execute-action 'helm-bibtex-insert-citation))
+
+(defun power-ref-edit-notes ()
+  (interactive)
+  (helm-exit-and-execute-action 'bibtex-completion-edit-notes))
+
+(defun power-ref-tag-entries ()
+  (interactive)
+  (helm-exit-and-execute-action 'org-ref-helm-tag-entries))
 
 ;;;###autoload
 (defun power-ref (&optional arg)
@@ -330,31 +407,24 @@ arguments, validate the bibtex file and check for bad links."
 			 (org-ref-split-and-strip-string (plist-get plist ':path)))
 		  (when (not (-contains? keys key))
 		    (setq keys (append keys (list key)))))))))
-	;; (let ((cite-links nil))
-	;;   (save-excursion
-	;; 	(goto-char (point-min))
-	;; 	(while (re-search-forward "cite.:\\|cite:" nil t)
-	;; 	  (forward-word)
-	;; 	  (let* ((match (thing-at-point 'symbol t))
-	;; 		 (key (replace-regexp-in-string "cite.:\\|cite:" "" match)))
-	;; 	    (cl-pushnew key cite-links :test #'equal))))
 	(helm :sources `(((name . "Citation links")
 			  (candidates . ,(mapcar (lambda (x)
 						   (format "%s" x))
 						 (nreverse keys))) ; TODO sort by most cited
 			  (candidate-transformer power-ref-propertize)
 			  ;; TODO use org-ref-cite-candidates instead
-			  (action . ,org-ref-bibtex-completion-actions))
+			  (action . ,power-ref-actions))
 			 ((name . "BibTeX entries")
 			  (init . bibtex-completion-init)
 			  (candidates . bibtex-completion-candidates)
 			  (filtered-candidate-transformer . helm-bibtex-candidates-formatter)
-			  (action . ,power-ref-completion-actions))
+			  (action . ,power-ref-actions))
 			 ((name . "Fallback options")
 			  (match (lambda (_candidate) t))
 			  (candidates . bibtex-completion-fallback-candidates)
 			  (action . bibtex-completion-fallback-action)))
 	      :buffer "*power ref*"
+	      :keymap power-ref-map
 	      :candidate-number-limit 1000)))))
 
 (provide 'citation-utils)
