44cbf7ea654d3ec2ee397468878d5f71993bfd3c
Jonathan <jagrg@users.noreply.github.com>
2016-10-07 11:30:44 +0100

    Remove obsolete function

--- lisp/citation-utils.el
+++ lisp/citation-utils.el
@@ -155,52 +155,6 @@ In `dired-mode', rename pdf file at point."
 	  (message (format "Created file %s" fname)))))))
 
 ;; ==================================================================
-;;;; local database
-;; ==================================================================
-
-(defun bibtex-completion--get-local-databases ()
-  "Return a list of .bib files associated with the current file.
-This function searches the current file or its master file for a
-`\\bibliography' or `\\addbibresource' command and returns the
-associated .bib file(s). If no files are found locally, return the
-files specified in the variable `bibtex-completion-bibliography'."
-  (let ((texfile nil)
-	(dir (file-name-directory (buffer-file-name)))
-	(cb (current-buffer)))
-    (when (and (boundp 'TeX-master)
-	       (stringp TeX-master))
-      (setq texfile (if (file-name-extension TeX-master)
-			TeX-master
-		      (concat TeX-master ".tex"))))
-    (with-temp-buffer
-      (if (and texfile (file-readable-p texfile))
-	  (insert-file-contents texfile)
-	(insert-buffer-substring cb))
-      (save-match-data
-	(goto-char (point-min))
-	(cond
-	 ;; bibtex
-	 ((re-search-forward "\\\\\\(?:no\\)*bibliography{\\(.*?\\)}" nil t)
-	  (mapcar (lambda (fname)
-		    (if (file-name-extension fname)
-			(format "%s%s" dir fname)
-		      (format "%s%s.bib" dir fname)))
-		  (split-string (match-string-no-properties 1) ",[ ]*")))
-	 ;; biblatex
-	 ((re-search-forward "\\\\addbibresource\\(\\[.*?\\]\\)?{\\(.*?\\)}" nil t)
-	  (mapcar (lambda (fname)
-		    (if (file-name-extension fname)
-			(format "%s%s" dir fname)
-		      (format "%s%s.bib" dir fname)))
-		  (let ((option (match-string 1))
-			(file (match-string-no-properties 2)))
-		    (unless (and option (string-match-p "location=remote" option))
-		      (split-string file ",[ ]*")))))
-	 (t
-	  bibtex-completion-bibliography))))))
-
-
-;; ==================================================================
 ;;;; labels and cross-referencing
 ;; ==================================================================
 
